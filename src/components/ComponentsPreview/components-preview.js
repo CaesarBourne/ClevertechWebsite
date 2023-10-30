5f4ccab8bba2cd8f06d25d3f1c17c9494220bea4
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-10-30 22:03:53 +0100

    test case passed some

--- src/components/ComponentsPreview/components-preview.js
+++ src/components/ComponentsPreview/components-preview.js
@@ -1,32 +1,36 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { ComponentsWrapper } from '../ComponentsWrapper/components-wrapper';
-import { componentsActions } from '../../store/components';
+import { ComponentsWrapper } from "../ComponentsWrapper/components-wrapper";
+import { componentsActions } from "../../store/components";
 
-import './components-preview.css'
-
-const isEmpty = obj => Object.keys(obj).length === 0;
+import "./components-preview.css";
 
 export const ComponentsPreview = () => {
-  const components = useSelector(state => state.components?.items);
-  const currentlyEdited = useSelector(state => state.components?.currentlyEdited);
-  const dispatch = useDispatch();  
-  const onComponentEdit = component => dispatch(componentsActions.setEditedComponent({component}));
+  const components = useSelector((state) => state.components.items);
+  const currentlyEdited = useSelector(
+    (state) => state.components.currentlyEdited
+  );
+  const dispatch = useDispatch();
+  const onComponentEdit = (component) =>
+    dispatch(componentsActions.setEditedComponent(component));
 
   return (
     <div data-testid="components-preview" className="components-preview">
-      {components?.map(component => (
+      {components.map((component) => (
         <ComponentsWrapper
           key={component.id}
-          isEdited={component?.id === currentlyEdited?.id && !isEmpty(component.values)}
+          isEdited={
+            component.id === currentlyEdited?.id &&
+            Object.keys(component.values).length > 0
+          }
           onComponentEdit={() => onComponentEdit(component)}
           {...component}
         />
       ))}
     </div>
   );
-}
+};
 
 ComponentsPreview.defaultProps = {
   currentlyEdited: {},
