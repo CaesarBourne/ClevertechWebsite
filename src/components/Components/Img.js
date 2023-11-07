2bc9861421868c3536d4fa7b461f80769abcf068
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-11-07 23:18:19 +0100

    currently edited values css

--- src/components/Components/Img.js
+++ src/components/Components/Img.js
@@ -3,5 +3,5 @@ import React from "react";
 export const Img = ({ values }) => {
   const { alt, src } = values;
 
-  return <img src={src} alt={alt} />;
+  return <img src={src} alt={alt} className="oka" />;
 };
